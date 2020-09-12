f7316cf367fac8c31e980ed05a027f4bab83c60a
afindlay7 <afindlay@deloitte.ca>
2020-09-12 09:36:41 -0400

    MTL: Contact form complete

--- src/app/shared/request-access/request-access.component.ts
+++ src/app/shared/request-access/request-access.component.ts
@@ -14,7 +14,7 @@ export class RequestAccessComponent implements OnInit {
     lastName: new FormControl(''),
     company: new FormControl(''),
     title: new FormControl(''),
-    // role: new FormControl(''),
+    role: new FormControl(''),
     email: new FormControl(''),
     message: new FormControl(''),
   });
@@ -26,14 +26,13 @@ export class RequestAccessComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.contactForm.value);
     const body = new HttpParams()
       .set('form-name', 'contact')
       .append('firstName', this.contactForm.value.firstName)
       .append('lastName', this.contactForm.value.lastName)
       .append('company', this.contactForm.value.company)
       .append('title', this.contactForm.value.title)
-      // .append('role', this.contactForm.value.role)
+      .append('role', this.contactForm.value.role)
       .append('email', this.contactForm.value.email)
       .append('message', this.contactForm.value.message);
     this.http.post('/', body.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).subscribe(
@@ -41,16 +40,16 @@ export class RequestAccessComponent implements OnInit {
       (err) => {
         if (err instanceof ErrorEvent) {
           // Client side error
-          alert('Something went wrong when sending your message.');
+          alert('Sorry - Something went wrong when sending your message.');
           console.log(err.error.message);
           this.activeModal.close('error');
         } else {
           // Backend error
           if (err.status === 200) {
-            alert('Your message has been sent!');
+            alert('Your message has been sent to the Mantle team!');
             this.activeModal.close('success');
           } else {
-            alert('Something went wrong when sending your message.');
+            alert('Sorry - Something went wrong when sending your message.');
             console.log('Error status:');
             console.log(err.status);
             console.log('Error body:');
