11297dd5b3d1f16f4b5fd61c7505de5aabef10da
afindlay7 <afindlay@deloitte.ca>
2020-09-15 12:56:31 -0400

    MTL: Customer testimonial section

--- src/app/shared/testimonials/testimonials.component.ts
+++ src/app/shared/testimonials/testimonials.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+  styleUrls: ['./testimonials.component.scss'],
 })
 export class TestimonialsComponent implements OnInit {
+  activeTestimonial = 0;
+  customerTestimonials = [
+    {
+      title: 'Insert testimonial 1 here. Lorem ipsum dolor sit amet, consecteur adipiscing elit, sed do eiusmod.',
+      label: 'BUZZ WORD',
+    },
+    {
+      title: 'Insert testimonial 2 here. Lorem ipsum dolor sit amet, consecteur adipiscing elit, sed do eiusmod.',
+      label: 'BUZZ WORD',
+    },
+    {
+      title: 'Insert testimonial 3 here. Lorem ipsum dolor sit amet, consecteur adipiscing elit, sed do eiusmod.',
+      label: 'BUZZ WORD',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onPrevious() {
+    if (this.activeTestimonial !== 0) {
+      this.activeTestimonial -= 1;
+    }
   }
 
+  onNext() {
+    if (this.activeTestimonial !== this.customerTestimonials.length - 1) {
+      this.activeTestimonial += 1;
+    }
+  }
 }
